b63d5e069c0d76dcd38910302a05b096ce390b04
gjing <me@gjing.cn>
2022-10-27 16:43:19 +0800

    删除order的多余vo

--- order/controller/vo/order_vo.go
+++ order/controller/vo/order_vo.go
@@ -1,11 +1,5 @@
 package vo
 
-type OrderList struct {
-	No     string  `json:"no"`
-	Price  float64 `json:"price"`
-	Status int     `json:"status"`
-}
-
 type OrderDetail struct {
 	Username string   `json:"username"`
 	Mobile   string   `json:"mobile"`
